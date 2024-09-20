0d1327b47fae7210b3dd30a32ff2d238c6c2143e
fenilbharodiya <bharodiyafenil1206@gmail.com>
2024-09-20 12:07:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import EventComponent from './Components/Event_Prop_Validation.js/Event';
-import EventComponent1 from './Components/Event_Prop_Validation.js/Event_With_params';
+
+import ClassComponent1 from './Components/ClassComponent.js/ClassComponent1';
 import ParentComponent from './Components/Event_Prop_Validation.js/ParentComponent';
 import StatehooksComponent from './Components/Hooks/StateHooksComponent';
-import ClassComponent1 from './Components/ClassComponent1';
-import Component1 from './Components/Function_1';
-import SchoolComponent from './Components/props.js/SchoolComponent';
 
 
 
 function App() {
   return (
     <>
-      {/* <ClassComponent1 />
-      <Component1 /> */}
-      {/* <SchoolComponent /> */}
-      {/* <EventComponent /> */}
+      <ClassComponent1 />
       {/* <ParentComponent /> */}
-      {/* <EventComponent1 /> */}
-      <StatehooksComponent />
+      {/* <StatehooksComponent /> */}
     </>
   );
 }
