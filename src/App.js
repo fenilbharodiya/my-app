af57f16888d18da15081423257f605fe9b65a75f
fenilbharodiya <bharodiyafenil1206@gmail.com>
2024-09-24 17:23:09 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import ClassComponent1 from './Components/ClassComponent.js/ClassComponent1';
 import ParentComponent from './Components/Event_Prop_Validation.js/ParentComponent';
 import FunctionComponent1 from './Components/FunctionComponent.js/Function_1';
-import ListandKeysComponent from './Components/Hooks/ListandKeysComponent';
+import ListandKeysComponent from './Components/List_and_keys/ListandKeysComponent';
 import StatehooksComponent from './Components/Hooks/StateHooksComponent';
 import UseEffectHookComponent from './Components/Hooks/UseEffectHookComponent';
 
