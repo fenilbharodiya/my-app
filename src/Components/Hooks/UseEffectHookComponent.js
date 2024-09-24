bab5ee5622bfde731873ec9163d6ee181eb410ae
fenilbharodiya <bharodiyafenil1206@gmail.com>
2024-09-24 17:02:08 +0530

    useeffect hooks dependency

--- src/Components/Hooks/UseEffectHookComponent.js
+++ src/Components/Hooks/UseEffectHookComponent.js
@@ -4,16 +4,41 @@ import { useState } from "react"
 
 export default function UseEffectHookComponent() {
     const [count, setcount] = useState(0)
+    const [name, setName] = useState('Default Name');
+    // useEffect(() => {
+    //     console.log("hello");
+    //     console.log(count);
+    // })
 
     useEffect(() => {
-        console.log("hello");
-        console.log(count);
-    })
+        console.log('Component Mounted!');
+        
+    },[])
+
+    useEffect(() => {
+        console.log(name);
+        
+    }, [name])
+
+
+
+    const handelInput = (e) => {
+        setName(e.target.value)
+    }
+
+    useEffect(()=>{
+        let timer = setTimeout(()=>{
+            setcount((count) => count + 2)
+        }, 2000);
+
+        return () => clearTimeout(timer)
+    }, [])
 
 
     return (
         <div>
             <button onClick={() => setcount(count + 1)}>increaseCount</button>
+            <input onChange={handelInput} value={name} />
             
         </div>
     )
