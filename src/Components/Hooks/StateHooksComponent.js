343555226fbdc96f27ddb373dce8658e597e396c
fenilbharodiya <bharodiyafenil1206@gmail.com>
2024-09-24 15:23:07 +0530

    ALL DEMO

--- src/Components/Hooks/StateHooksComponent.js
+++ src/Components/Hooks/StateHooksComponent.js
@@ -2,6 +2,12 @@ import { useState } from "react";
 
 export default function StatehooksComponent () {
     let [count, setcount] = useState(0);
+    let components = {
+        ram: 512,
+        cpu: 'i5 4th Gen',
+    }
+    let [getPCParts, setPCParts] = useState(components);
+
     let increase = () => setcount(count + 1)
 
 
